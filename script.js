8e68014e592e68069226104240eddc542216d3b0
MoSayed2008 <ahmedmido66112@gmail.com>
2024-08-15 18:43:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,13 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+        },
+        5: {
+            name: 'Merge Away',
+            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4'
         }
+        
     };
 
     const startBtn = document.getElementById('startBtn');
@@ -221,9 +227,8 @@ document.addEventListener('DOMContentLoaded', () => {
         // Show the form sections again
         gameSelectGroup.style.display = 'block';
         keyCountGroup.style.display = 'block';
-        
-        
     });
+
 
     const generateClientId = () => {
         const timestamp = Date.now();
